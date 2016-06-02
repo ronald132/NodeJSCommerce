049335293ed8a186a7194f51673b39e71555a4ac
ronald132 <RSitumorang@FICSGMAC01.local>
2016-06-02 18:20:34 +0800

    Category added add category page added add category routes

--- server.js
+++ server.js
@@ -50,8 +50,10 @@ app.set('view engine', 'ejs');
 
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
+var adminRoutes = require('./routes/admin');
 app.use(mainRoutes);
 app.use(userRoutes);
+app.use(adminRoutes);
 
 //start server at port 3000
 app.listen(secret.port, function(err){
